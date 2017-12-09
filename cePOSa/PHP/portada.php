7fea4ab4700123c7a40e5597d353ecad2bc159cc
Vladimir Hidalgo <vladimiroski@gmail.com>
2017-12-09 05:38:42 -0600

    Primera mejora Dic 17

--- cePOSa/PHP/portada.php
+++ cePOSa/PHP/portada.php
@@ -10,65 +10,43 @@
 </table>
 <hr />
 
-<p style="text-align: center; margin: 10px 0;">© 2010 FLOR360.COM - Floristerias El Salvador® - <strong><?php echo PROY_TELEFONO_PRINCIPAL; ?></strong> - <a href="ayuda?tema=terminos_y_condiciones">terminos y condiciones</a>
+<div id="pie-pagina">
+    <p style="text-align: center; margin: 10px 0;">Copyright © 2010 FLOR360.COM</p>
+    <p style="text-align: center; margin: 10px 0;">© 2010 FLOR360.COM - Floristerias El Salvador® - Telefonos: (503) 2278-8391 ; (503) 2243-6017</p>
 
-<p style="text-align: justify;">Flor360.com te pone a dispocisión
-cualquier tipo de arreglo floral, detalle o regalo; contamos con
-los mas especiales arreglos utilizando para esto flores&nbsp;
-naturales con diseños que le dan un giro a la imaginación y
-que hacen de todos nuestros productos el regalo más exclusivo y
-especial que recibira esa persona tan importante en su vida.</p>
-<p>Simplemente no encontrará un mejor lugar para ordenar flores en
-línea en El Salvador. Estaremos contigo en cada día especial o
-simplemente recordarle a un ser querido cuanto lo aprecias.</p>
+    <table style="width: 100%;">
+        <tr>
+            <td style="width: 40%">
+                <p>
+                    Somos una empresa de reconocido prestigio, en flor360.com contamos con mas de 10 años dedicándonos al diseño floral, con servicios de entrega a domicilio,
+                    entregas disponibles para el mismo día o entrega para el siguiente día. Contamos además con servicio de decoración <a href="#">...+MAS INFO</a>
+                </p>
+            </td>
+            <td style="width: 40%;">
+                <p class="pie-titulo" style="text-align: center;">
+                    EMPRESAS DE NUESTRO GRUPO
+                </p>
+                <br />
+                <p  style="text-align: center;">
+                    <a href="#">
+                        <img src="IMG/portada/condolencias.link.jpg" />
+                    </a>
+                    <a href="#">
+                        <img src="IMG/portada/eventos360.link.jpg" />
+                    </a>
+                </p>
+            </td>
+            <td style="width: *;text-align: center;">
+                <p class="pie-titulo">
+                    ...+MAS INFORMACION
+                </p>
+                <p>
+                    - Terminos y condiciones<br />
+                    - Reclamos, quejas o sugerencias<br />
+                    - Politica de privacidad
+                </p>
 
-<p style="text-align: justify;">Nuestros servicios: arreglos
-florales en El Salvador, floristeria en El Salvador, globos, San
-Valentin, Dia de la Madre, decoraciones para boda, quince años,
-eventos empresariales, eventos familiares, graduaciones, centros de
-mesa y arreglos frutales.</p>
-
-<p style="text-align: justify;">En flor360.com somos importadores
-directos de flores, lo cual asegura no solamente&nbsp; que los
-regalos que envies serán entregados a tiempo, sino tambien
-significa que el precio de nuestros arreglos es el mas bajo del
-mercado y que el producto que te entregaremos ha sido preparado con
-las flores más frescas como sea posible&nbsp; y ofrecen un gran
-valor para la persona que lo recibe como para quien lo envía.</p>
-
-<p style="text-align: justify;">Nuestro equipo de atención al
-cliente esta preparado para ayudarte con cualquier orden. Ya sea
-que envías una&nbsp; rosa para celebrar tu amor o una canasta de
-regalo para felicitar a un compañero de trabajo; si tienes
-preguntas nuestro equipo tiene las respuestas. Llamanos al
-<?php echo PROY_TELEFONO_PRINCIPAL; ?> o haz tu orden en línea. Nosotros te ayudaremos a
-encontrar y entregar las flores adecuadas, plantas o regalos para
-celebrar los momentos mas significativos de la vida.</p>
-
-<p style="text-align: justify;">Tu mejor opción cuando compras en
-línea por flores, ramos y regalos en cualquier momento del año,
-especialmente en el día de San Valentin y el Día de las Madres.
-Tenemos una maravillosa selección de flores y plantas frescas
-incluyendo rosas, gerberas, orquidias, tulipanes, lirios, y más.
-Ademas ofrecemos deliciosas canastas de regalo de fruta fresca,
-comida gourmet, chocolates y dulces.</p>
-
-<p style="text-align: justify;">También encontrarás ramos para
-todas las ocasiones: cumpleaños, aniversarios, Solo porque Si,
-Nuevo Bebe, Amor y Romance, Lo siento, Que te mejores, Simpatía y
-condolencias.&nbsp; Ya sea que este buscando por un ramo de flores
-o una planta de maceta, nuestra variedad de flores y arreglos le
-proveeran de suficientes opciones. Las entregas del mismo dia estan
-disponibles.</p>
-
-<p style="text-align: justify;">En 2010, Flor360.com, abre su
-primera floristería y cambió para siempre la forma en que las
-flores son compradas para cumpleaños, aniversarios y ocasiones
-especiales. nuestra pasión ha sido ayudarte a conectarte y
-expresarte a la gente importante en tu vida a travez de la
-selección mas fina de bellas flores y arreglos florales, desde
-rosas a tulipanes a gerberas a orquidias a fullys o gerberas,
-disponibles para el mismo día o entrega para el siguiente día.
-Tambien puedes encontrar plantas, canastas de regalo, comida
-gourmet, animalitos de peluche perfectos para cada ocasion y
-cubiertos por nuestras Garantía de Satisfacción Total.</p>
+            </td>
+        </tr>
+    </table>
+</div>
